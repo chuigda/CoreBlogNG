651eb13f0f04d9c7350535e69e7addf610a5b719
icey <icey@icey.tech>
2023-01-18 16:43:09 +0800

    Fix

--- src/views/blog-list.tsx
+++ src/views/blog-list.tsx
@@ -15,7 +15,7 @@ const BlogList = ({ liberal }) => {
       blogs.forEach(({ title, brief, time, timestamp, ident, author }) => {
          blogList.appendChild(
             <div class="blog-item">
-               <h2><a href={`/#/blog/${timestamp}-${ident}`}>{title}</a></h2>
+               <h2><div onClick={() => window.location.hash = `/blog/${timestamp}-${ident}`}>{title}</div></h2>
                <div class="brief">{brief}</div>
                <div class="author-line">由 <b>{author}</b> 发布于 { dayjs(time).format('YYYY 年 M 月 D 日 HH:mm:ss') }</div>
             </div>
