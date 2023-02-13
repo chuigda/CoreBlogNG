018e056fca555e02855551a0bd9f77146bba1c13
icey <icey@icey.tech>
2023-02-13 16:31:33 +0800

    Push JS and CSS into single file to speed up loading even more

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
    base: '',
+   plugins: [viteSingleFile()],
    build: {
       target: 'es2018',
       cssCodeSplit: false,
